ddb9651b12b87976bb5721658dfe10709ac2ab50
Julian Blaschke <julianblaschke@icloud.com>
2026-03-18 17:55:01 +0100

    fix: refactored leaderboard tests for updated game result sorting

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -38,20 +38,20 @@ class LeaderboardControllerTests {
     }
 
     @Test
-    fun test_getLeaderboard_sameScore_CorrectIdSorting() {
-        val first = GameResult(1, "first", 20, 20.0)
-        val second = GameResult(2, "second", 20, 10.0)
-        val third = GameResult(3, "third", 20, 15.0)
+    fun test_getLeaderboard_sameScore_CorrectTimeSorting() {
+        val slowest = GameResult(1, "first", 20, 20.0)
+        val fastest = GameResult(2, "second", 20, 10.0)
+        val middle = GameResult(3, "third", 20, 15.0)
 
-        whenever(mockedService.getGameResults()).thenReturn(listOf(second, first, third))
+        whenever(mockedService.getGameResults()).thenReturn(listOf(middle, slowest, fastest))
 
         val res: List<GameResult> = controller.getLeaderboard()
 
         verify(mockedService).getGameResults()
         assertEquals(3, res.size)
-        assertEquals(first, res[0])
-        assertEquals(second, res[1])
-        assertEquals(third, res[2])
+        assertEquals(fastest, res[0])
+        assertEquals(middle, res[1])
+        assertEquals(slowest, res[2])
     }
 
 }
